32fac29a9166fa2faa2581395a7413c4f4373072
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-11-25 23:18:33 -0600

    Se corrigue error, se modifica la vista y se configura la lista

--- src/main/java/com/montelonogo/Tickets/controlador/IndexControlador.java
+++ src/main/java/com/montelonogo/Tickets/controlador/IndexControlador.java
@@ -1,14 +1,64 @@
 package com.montelonogo.Tickets.controlador;
 
+import com.montelonogo.Tickets.modelo.ticket;
+import com.montelonogo.Tickets.servicio.TicketServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.*;
+
 @Component
 public class IndexControlador implements Initializable {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(IndexControlador.class);
+
+    @Autowired
+    private TicketServicio ticketServicio;
+
+    @FXML
+    private TableView<ticket> ticketTabla;
+
+    @FXML
+    private TableColumn<ticket, Integer> idTicketColumna;
+    @FXML
+    private TableColumn<ticket, String> ticketColumna;
+    @FXML
+    private TableColumn<ticket, String> responsableColumna;
+    @FXML
+    private TableColumn<ticket, String> estatusColumna;
+
+    private final ObservableList<ticket> ticketList =
+            FXCollections.observableArrayList();
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        ticketTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+         configurarColumna();
+         listarTickets();
+    }
 
+    private void listarTickets() {
+
+        logger.info("Ejecutando listado de tickets");
+        ticketList.clear();
+        ticketList.addAll(ticketServicio.listarTicket());
+        ticketTabla.setItems(ticketList);
+    }
+
+    private void configurarColumna() {
+        idTicketColumna.setCellValueFactory(new PropertyValueFactory<>("idTicket"));
+        ticketColumna.setCellValueFactory(new PropertyValueFactory<>("actividadTicket"));
+        responsableColumna.setCellValueFactory(new PropertyValueFactory<>("resposableTicket"));
+        estatusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
     }
 }
--- src/main/java/com/montelonogo/Tickets/servicio/TicketServicio.java
+++ src/main/java/com/montelonogo/Tickets/servicio/TicketServicio.java
@@ -2,6 +2,7 @@ package com.montelonogo.Tickets.servicio;
 
 import com.montelonogo.Tickets.modelo.ticket;
 import com.montelonogo.Tickets.repositorio.TicketRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 @Service
 public class TicketServicio implements ITicketServicio{
 
-
+    @Autowired
     private TicketRepositorio ticketRepo;
 
     @Override
